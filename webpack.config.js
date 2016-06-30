abcb2a559146ea85c3522fae9e4614b6fecf4d94
klingac <devklingac@gmail.com>
2016-06-30 14:40:39 +0200

    pridane HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
-//const validate = require('webpack-validator');
+const validate = require('webpack-validator');
+
+const parts = require('./libs/parts');
 
 const PATHS = {
     app: path.join(__dirname, 'app'),
@@ -34,7 +36,16 @@ switch(process.env.npm_lifecycle_event) {
         break;
     default:
         config = merge(common, {});
+
+	config = merge(
+		common,
+		parts.devServer({
+			// Customize host/port here if needed
+			host: process.env.HOST,
+			port: process.env.PORT
+		})
+	);
 }
 
-//module.exports = validate(config);
+module.exports = validate(config);
 module.exports = config;
