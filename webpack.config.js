45d7e79ec8617ef1d0707b4f56e787a3ea941f5d
klingac <devklingac@gmail.com>
2016-07-08 19:24:35 +0200

    oddelenie css do samostatneho suboru

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ const parts = require('./libs/parts');
 
 const PATHS = {
     app: path.join(__dirname, 'app'),
+    style: path.join(__dirname, 'app', 'main.css'),
     build: path.join(__dirname, 'build')
 };
 
@@ -15,6 +16,7 @@ const common = {
     // We'll be using the latter form given it's
     // convenient with more complex configurations.
     entry: {
+        style: PATHS.style,
 		app: PATHS.app
 		//,vendor: ['react']
 	},
@@ -58,7 +60,7 @@ switch(process.env.npm_lifecycle_event) {
 		}),
 
 		parts.minify(),
-        parts.extractCSS(PATHS.app)
+        parts.extractCSS(PATHS.style)
 	);
         break;
     default:
@@ -68,7 +70,7 @@ switch(process.env.npm_lifecycle_event) {
 		{
 			devtool: 'eval-source-map'
 		},
-		parts.setupCSS(PATHS.app),
+		parts.setupCSS(PATHS.style),
 		parts.devServer({
 			// Customize host/port here if needed
 			host: process.env.HOST,
